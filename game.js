324f35ba2ef7560d140b8a3bd01fc2675167430f
Wojciech Czartoryjski <wczartoryjski@gmail.com>
2021-05-23 15:41:26 +0200

    display shuffled board

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const button = document.querySelector("button");
 const timer = document.querySelector('#timer');
 let cards = document.querySelectorAll("div");
 cards = [...cards];
+console.log(cards);
 
 let currentTime = 0;
 let currentTime2 = 0;
@@ -15,33 +16,35 @@ console.log(board);
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
-
         let j = Math.floor(Math.random() * (i + 1));
-        
         let temp = array[i];
-
         array[i]=array[j];
         array[j]=temp;
-
     }
     return array;
 }
 
-console.log(shuffle(board));
-
+// console.log(shuffle(board));
 //koniec tasowania
 
-
+//odliczanie czasu (wrazliwe na multiclick)
 function countUp() {
     currentTime++
     timer.textContent = currentTime +' s'
 }
+//koniec odliczania czasu
 
+//funkcjonalnosc przycisku start/restart
 const initialization = function() {
     const startTime = new Date().getTime();
     let countUpTimer = setInterval(countUp,1000)
-
+    shuffle(board);
+    console.log(shuffle(board));
+    for (let i = 15;i>=0; i--){
+        cards[i].textContent = board[i];
+    }
 }
+//koniec dzialania przycisku
 
 //nasluchiwanie na klik
 button.addEventListener("click", initialization)
